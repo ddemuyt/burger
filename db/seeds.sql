87cba23a6df6a3658fce52b7dc0e455c12f39f81
Drake <drakedemuyt@gmail.com>
2020-05-04 03:01:54 -0400

    Adding server.js, config, and db files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Western Burger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
