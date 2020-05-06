d7bb8cf7e49c68d0780aba6130cc0c756b8a1fca
Drake <drakedemuyt@gmail.com>
2020-05-06 00:50:16 -0400

    Working with JS files to listen to button clicks

---
+++ public/assets/js/burger_script.js
@@ -0,0 +1,44 @@
+$(function() {
+    $(".devourBtn").on("click", function(event) {
+      var id = $(this).data("id");
+      var eaten = $(this).data("eaten");
+  
+      var newEatenState = {
+        devoured: eaten
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/cats/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("changed sleep to", eaten);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burg").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("added a new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+  });
